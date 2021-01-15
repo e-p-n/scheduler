a1d4c3e9335a72ecfb337cc2b8bbe18a46ab2559
Eric Normann <eric.n@me.com>
2021-01-15 01:30:20 -0500

    complete functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,37 @@
+// loads local storage of schedule into array 
+var scheduleItems = JSON.parse(localStorage.getItem("schedule"));
+if (!scheduleItems) {
+    scheduleItems = [];
+}
+
+// get and set today's date using moment.js. Display on screen
 var today = moment().format("dddd, MMMM Do");
 $("#currentDay").text(today);
 
-function buildCalendar() {
-    let currentHour = moment().subtract(6, 'hours').format("H");
-    
+// compare the date saved in local storage with today's date. If they don't match, reset the scheduleItems array to empty.
+function dateCheck() {
+    let savedDate = localStorage.getItem("date");
+    if (savedDate != today) {
+        scheduleItems = [];
+    }
+    buildSchedule();
+}
+
+function buildSchedule() {
+    // save today's date in local storage
+    localStorage.setItem("date", today);
+
+    // set the current hour using moment.js
+    let currentHour = moment().format("H");
     let meridiem = "AM";
     let textAreaClass = "";
+    let textAreaContents;
 
-    
+    // run through for loop and add schedule line for every hour
     for (var i=9; i <=17; i++) {
         let calTime = i;
+        textAreaContents = "";
+        // set the class for the textarea depending on time of day
         if (calTime < currentHour) {
             textAreaClass = "past";
         } else if (calTime == currentHour) {
@@ -17,6 +39,7 @@ function buildCalendar() {
         } else if (calTime > currentHour) {
             textAreaClass = "future";
         }
+        // format time to fit 12 hour clock
         if (calTime < 12) {
             meridiem = "AM";
         } else {
@@ -25,6 +48,8 @@ function buildCalendar() {
                 calTime -= 12;
             }
         }
+        // create form element and display it on screen. Add an event listener for each button
+        let textId = "ta-" + calTime;
         let formEl = $("<form/>",
                      {"class": "timeblock row"},
                      {"id": i});
@@ -32,20 +57,44 @@ function buildCalendar() {
                           {"class": "hour col-1"});
         timeBlockEl.append(calTime+meridiem);
         let textAreaEl = $("<textarea />",
-                         {"class": textAreaClass + " col-10"},
-                         {"id": "ta-"+i});
-        textAreaEl.text("Text goes here" + i);
+                         {"class": textAreaClass + " col-10", "id": textId});
+
+        // loop through scheduleItems array and if it's id matches textId load it's conents into the textarea
+        for (var j = 0; j < scheduleItems.length; j++) {
+            if (scheduleItems[j][0] === textId) {
+                textAreaContents = scheduleItems[j][1];
+            }
+        }
+        textAreaEl.text(textAreaContents);
         let submitBtn = $("<div />",
                         {"class": "saveBtn col-1"})
-        submitBtn.html('<submit><i id="sb-' + i + '" class="fas fa-save"></i></submit>');
+        submitBtn.html('<submit><i id="sb-' + calTime + '" class="fas fa-save"></i></submit>');
         formEl.append(timeBlockEl);
         formEl.append(textAreaEl);
         formEl.append(submitBtn);
         $(".container").append(formEl);
+
+        $("#sb-"+calTime).click(function(event) {
+            event.preventDefault();
+            saveText(textId);
+        });
+
     }
 }
 
-buildCalendar();
-$("submit").click(function() {
-    console.log("Clicked:" + $(this).id);
-})
+function saveText(schedId) {
+    let scheduleItemText = document.getElementById(schedId).value;
+    let scheduleItem = [schedId, scheduleItemText];
+    for (var i = 0; i < scheduleItems.length; i++) {
+        if (scheduleItems[i][0] === scheduleItem[0]) {
+            scheduleItems.splice(i, 1);
+        }
+    }
+    scheduleItems.push(scheduleItem);
+    localStorage.setItem("schedule", JSON.stringify(scheduleItems));
+
+
+}
+
+dateCheck();
+
